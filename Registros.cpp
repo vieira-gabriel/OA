43063695f0b360ec2aa2109e986295dd330e0394
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2018-05-27 00:23:15 -0300

    Agora compilando

--- Registros.cpp
+++ Registros.cpp
@@ -1,5 +1,24 @@
 #include "Registros.hpp"
 
+void ArquivoDeIndice::visualizar(list<IndiceP>& primario, list<IndiceS>& secundario){
+	list<IndiceP>::iterator temp1;
+
+	cout << "\tChaves secunárias" << endl << endl;
+	for(list<IndiceS>::iterator ponteiroS = secundario.begin(); ponteiroS != secundario.end(); ++ponteiroS){
+		temp1 = ponteiroS->registroP;
+    	cout << ponteiroS->curso << ' ' << &temp1 << endl;
+	}
+
+
+    cout << "\tChaves primárias" << endl << endl;
+	for(list<IndiceP>::iterator ponteiroP = primario.begin(); ponteiroP != primario.end(); ++ponteiroP){
+		temp1 = ponteiroP->registro;
+    	cout << &ponteiroP << ' ' << ponteiroP->identificador << ' ' << &temp1 << endl;
+	}
+
+    cout << endl;
+}
+
 void ArquivoDeIndice::incluir(list<IndiceP>& primario, list<IndiceS>& secundario, IndiceP Chave1, IndiceS Chave2){
 	list<IndiceP>::iterator ponteiroSP;
 	list<IndiceP>::iterator ponteiroP;
@@ -7,34 +26,36 @@ void ArquivoDeIndice::incluir(list<IndiceP>& primario, list<IndiceS>& secundario
 
 	// Incerindo a chave na lista secundária
 	ponteiroS = secundario.begin();									// Ponteiro aponta pro primeiro elemento da lista secundaria
-	while(*ponteiroS != Chave2 || ponteiroS != secundario.end()){	// Laço de repetição para verificar se a chave ja n existe na lista até que o ponteiro chegue no final da mesma
-		if (*ponteiroS == Chave2) break; 							// Caso ja exista a chave, sai do laço e o ponteiro é salvo para mais tarde
+	while(ponteiroS->curso != Chave2.curso || ponteiroS != secundario.end()){	// Laço de repetição para verificar se a chave ja n existe na lista até que o ponteiro chegue no final da mesma
+		if (ponteiroS->curso == Chave2.curso) break; 							// Caso ja exista a chave, sai do laço e o ponteiro é salvo para mais tarde
 		ponteiroS++;												// Aponta para proximo elemento
 	}
-	if(ponteiroS == secundario.end()) secundario.incert(ponteiroS, Chave2);	// Incere chave no ultimo elemento da lista se ele ja não existir nela
+	if(ponteiroS == secundario.end()) secundario.insert(ponteiroS, Chave2);	// Incere chave no ultimo elemento da lista se ele ja não existir nela
 
 	// Incerindo a chave na lista primária
-	ponteiroSP = *ponteiroS.registroP;
-	if(ponteiroSP != NULL){				// Se a chave secundária não aponta para nenhum elemento na lista primária
-		ponteiroP = ponterioSP;
-		while(ponteiroP != NULL || *ponteiroP != Chave1) ponteiroP = *ponteiroP.registro;	// Verifica se a chave não existe na lista
-		if (*ponteiroP == Chave1) break;			// Caso já exista a chave, não realiza mais nada;
+	ponteiroSP = ponteiroS->registroP;
+	if(ponteiroSP != primario.end()){				// Se a chave secundária não aponta para nenhum elemento na lista primária
 		ponteiroP = ponteiroSP;
-		while(ponteiroP != NULL){		// Enquanto não chegar no ultimo elemento referente à chave secundária na lista primária
-			// Compara se a chave 1 é maior ou menor para ja incerir na ordem certa
-			ponteiroP = *ponteiroP.registro;
+		while(ponteiroP != primario.end() || ponteiroP->identificador != Chave1.identificador) ponteiroP = ponteiroP->registro;	// Verifica se a chave não existe na lista
+		if (ponteiroP->identificador == Chave1.identificador);			// Caso já exista a chave, não realiza mais nada;
+		else{
+			ponteiroP = ponteiroSP;
+			while(ponteiroP != primario.end()){		// Enquanto não chegar no ultimo elemento referente à chave secundária na lista primária
+				// Compara se a chave 1 é maior ou menor para ja incerir na ordem certa
+				ponteiroP = ponteiroP->registro;
+			}
 		}
 	}
 	else{									// Se a chave secundária aponta para nenhum elemento na lista primária, a chave primaria sera incerida no final da lista
 		ponteiroP = primario.end();
-		primario.incert(ponteiroP, Chave1);
-		Chave2.registro = ponteiroP;
+		primario.insert(ponteiroP, Chave1);
+		Chave2.registroP = ponteiroP;
 	}
 
 	//Aqui entra algoritmo de ordenação lista secundária
 }
 
-void ArquivoDeIndice::criar(list<IndiceP>& primario, list<IndiceS>& secundario, stirng arquivo){
+void ArquivoDeIndice::criar(list<IndiceP>& primario, list<IndiceS>& secundario, char arquivo[]){
 	ifstream File;
 	string matricula, nome, curso, chaveP, aux, turma;
 	IndiceP Chave1;
--- Registros.hpp
+++ Registros.hpp
@@ -1,3 +1,6 @@
+#ifndef REGISTROS_H
+#define REGISTROS_H
+
 #include<stdio.h>
 #include<stdlib.h>
 #include <string>
@@ -12,24 +15,26 @@ using namespace std;
 
 typedef struct RegistroP{
 	string 	identificador;							// Esse identificador vai ser a matricula mais as iniciais do nome
-	list<IndiceP>::iterator registro;				// Ponteiro para próximo elemento referente ao índice secundário
+	list<RegistroP>::iterator registro;				// Ponteiro para próximo elemento referente ao índice secundário
 }IndiceP;
 
 typedef struct RegistroS{
-	char curso[2];
+	string curso;
 	list<IndiceP>::iterator registroP;				// Ponteiro para a lista de indice primario
 }IndiceS;
+
+
 class ArquivoDeIndice{
 private:
 	list<IndiceP> primario;
 	list<IndiceS> secundario;
 public:
+	void visualizar(list<IndiceP>&, list<IndiceS>&);
 	void incluir(list<IndiceP>&, list<IndiceS>&, IndiceP, IndiceS);
-	void criar(list<IndiceP>&, list<IndiceS>&, string);
+	void criar(list<IndiceP>&, list<IndiceS>&, char*);
 	void excluir(list<IndiceP>&, list<IndiceS>&);
 	void atualizar(list<IndiceP>&, list<IndiceS>&);
-	void visualizar(list<IndiceP>&, list<IndiceS>&);
-	ArquivoDeIndice();
-	~ArquivoDeIndice();
 	
 };
+
+#endif
